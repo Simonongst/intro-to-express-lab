64ac27bc678c5d4e8ef761cb249c5aa60e0444bb
simonongst@gmail.com <simonongst@Simons-Mac-mini.local>
2025-10-08 17:30:54 +0800

    feat: 1. Be Polite, Greet the User

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+// 1. Be Polite, Greet the User
+app.get("/greetings/:username", (req, res) => {
+  res.send(
+    `<h1>What a delight it is to see you once more, ${req.params.username}.</h1>`
+  );
+});
+
 app.listen(3000, () => {
-    console.log("Listening to port 3000");
+  console.log("Listening to port 3000");
 });
