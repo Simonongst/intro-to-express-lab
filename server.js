1178bf66ad909ac68f9d52a1334d38b2f3415cab
simonongst@gmail.com <simonongst@Simons-Mac-mini.local>
2025-10-08 23:17:49 +0800

    feat: 4. Filter Shoes by Query Parameters

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.get("/roll/:number", (req, res) => {
 });
 
 // 3. I Want THAT One!
-
 const collectibles = [
   { name: "shiny ball", price: 5.95 },
   { name: "autographed picture of a dog", price: 10 },
@@ -29,7 +28,6 @@ const collectibles = [
 ];
 
 app.get("/collectibles/:index", (req, res) => {
-
   const index = Number(req.params.index);
 
   if (isNaN(index) || index < 0 || index > collectibles.length - 1) {
@@ -37,8 +35,45 @@ app.get("/collectibles/:index", (req, res) => {
   }
 
   const item = collectibles[index];
-  res.send(`So, you want the ${item.name}? For ${item.price}, it can be yours!`)
-  
+  res.send(
+    `So, you want the ${item.name}? For ${item.price}, it can be yours!`
+  );
+});
+
+// 4. Filter Shoes by Query Parameters
+const shoes = [
+  { name: "Birkenstocks", price: 50, type: "sandal" },
+  { name: "Air Jordans", price: 500, type: "sneaker" },
+  { name: "Air Mahomeses", price: 501, type: "sneaker" },
+  { name: "Utility Boots", price: 20, type: "boot" },
+  { name: "Velcro Sandals", price: 15, type: "sandal" },
+  { name: "Jet Boots", price: 1000, type: "boot" },
+  { name: "Fifty-Inch Heels", price: 175, type: "heel" },
+];
+
+app.get("/shoes", (req, res) => {
+    const minPrice = Number(req.query["min-price"]);
+    const maxPrice = Number(req.query["max-price"]);
+    const type = req.query.type;
+
+    let filteredShoes = shoes;
+
+    if(!isNaN(minPrice)){
+    filteredShoes = filteredShoes.filter(shoe => shoe.price >= minPrice);
+    }
+
+    if(!isNaN(maxPrice)){
+    filteredShoes = filteredShoes.filter(shoe => shoe.price <= maxPrice);
+    }
+
+    if(type){
+    filteredShoes = filteredShoes.filter(shoe => shoe.type === type)
+    }
+
+    const result = filteredShoes.map(shoe => `Name: ${shoe.name}, Price: ${shoe.price}, Type: ${shoe.type}`
+    ).join("<br>");
+
+    res.send(result || "No shoes match your criteria.")
 });
 
 app.listen(3000, () => {
