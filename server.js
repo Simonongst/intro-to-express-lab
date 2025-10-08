74fec9d83b41fad84f32f66b621653e42b97adb7
simonongst@gmail.com <simonongst@Simons-Mac-mini.local>
2025-10-08 17:31:31 +0800

    feat: 2. Rolling the Dice

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.get("/greetings/:username", (req, res) => {
   );
 });
 
+// 2. Rolling the Dice
+app.get("/roll/:number", (req, res) => {
+  const num = Number(req.params.number);
+
+  if (isNaN(num)) {
+    return res.send("You must specify a number.");
+  }
+
+  const roll = Math.floor(Math.random() * (num + 1));
+  res.send(`You rolled a ${roll}.`);
+});
+
 app.listen(3000, () => {
   console.log("Listening to port 3000");
 });
